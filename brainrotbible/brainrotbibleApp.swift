fd6f854dfa4539c249e36149251a983e06bad1cd
Mark Mauro <Mark_Mauro@anfcorp.com>
2024-10-02 14:47:13 -0400

    Initial Commit

---
+++ brainrotbible/brainrotbibleApp.swift
@@ -0,0 +1,17 @@
+//
+//  brainrotbibleApp.swift
+//  brainrotbible
+//
+//  Created by Mark Mauro on 10/2/24.
+//
+
+import SwiftUI
+
+@main
+struct brainrotbibleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
